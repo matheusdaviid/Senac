1f20942d89348d988253714b15b43798e34dae64
matheusdaviid <matheus971592191@gmail.com>
2024-09-26 16:49:44 -0300

    Add files via upload

---
+++ UC 09/Primeiras aulas de Javascript/Aula 1/main.js
@@ -0,0 +1,39 @@
+//variavel é usada como algo que muda
+console.log("ola mundo")
+ 
+//texto -> string (guarda informaçoes)
+//numeros inteiros -> int (armazenas inteiros 1,2,3)
+//numeros decimais -> floot / double (floot guarda menos/double guarda mais informaçoes )
+//logicos -> boolean (representa o sim/não) em ingles seria (true or false)
+ 
+  //  var telefone = "19 997743035" // as ("") significa que é uma string
+  //  var valormonetario = 50.59
+  //  var valorinteiro = 13
+  //  var truefalse = true
+ 
+//texto -> Guarde os dados do seu endereço em VARIAVEIS (ex. rua, bairro, cep, estado...)
+// Guarde os seus dados (nome, email, telefone, data de nascimento,genero) em VARIAVEIS
+//Crie uma VARIAVEL que diz que se voce é maior de idade
+//Crie uma VARIAVEL que diz se você pode dirigir
+ 
+ 
+var rua = "São luis"
+var bairro = "Planalto do sol,"
+var cep = "13454350"
+var estado = "São Paulo"
+     console.log ("")
+console.log (rua, bairro, cep, estado)
+ 
+var  nome = "Matheus David"
+var  email = "matheus.david@senacsp.edu.br"
+var  telefone = "19 997743035"
+var  genero = "homem"
+ 
+     console.log (nome)
+     console.log (email)
+     console.log (telefone)
+     console.log (genero)
+     console.log ("")
+var  maior_de_idade = "false"
+ console.log (maior_de_idade)
+
