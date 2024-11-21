33e0bd57c4df981789ac12f384404e447a3b6f40
Matheus David <Matheus971592191@gmail.com>
2024-11-21 11:56:44 -0300

    ...

--- UC15/CineBox/assets/js/offcanvas.js
+++ UC15/CineBox/assets/js/offcanvas.js
@@ -1,8 +1,9 @@
 function abrirNav(){
 const menu = document.getElementById("offcanvas")
-
+menu.style.width="250px"
 }
 
 function fecharNav(){
-
+const menu=document.getAnimationsElementById("offcanvas")
+menu.style.width="0"
 }
